cc3ef1caf014e32e56d5ebfd3230f731fc4bf439
pysan3 <pysan3@gmail.com>
2023-08-09 01:56:46 +0900

    refactor(data_utils): less scope of `rich` requirement

--- mltemplate/utils/data_utils.py
+++ mltemplate/utils/data_utils.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 from typing import Optional
 
-from rich.progress import track
-
 
 def local_iter(start: int, end: int, step: int, local_rank: Optional[int] = None, desc: Optional[str] = None):
     if local_rank == 0:
+        from rich.progress import track
+
         return track(range(start, end, step), description=desc or "Working...")
     else:
         return range(start, end, step)
